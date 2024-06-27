422f1aec00e8bf09ad088040cef62e4842e630f5
patel-param-09 <patelparam9304@gmail.com>
2024-06-27 11:55:57 +0530

    1.3: Course Information step 3

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,29 @@ import Content from "./components/Content";
 import Total from "./components/Total";
 
 function App() {
-  return (
-    <div>
-      <Header course={course} />
-      <Content />
-      <Total />
-    </div>
-  );
+  const App = () => {
+    const course = "Half Stack application development";
+    const part1 = {
+      name: "Fundamentals of React",
+      exercises: 10,
+    };
+    const part2 = {
+      name: "Using props to pass data",
+      exercises: 7,
+    };
+    const part3 = {
+      name: "State of a component",
+      exercises: 14,
+    };
+
+    return (
+      <div>
+        <Header course={course} />
+        <Content />
+        <Total />
+      </div>
+    );
+  };
 }
 
 export default App;
